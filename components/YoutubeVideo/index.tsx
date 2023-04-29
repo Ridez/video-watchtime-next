30a7daed3a564c608fc556d65b6139c1d23e89b8
Lukasz Lassota <lukasz.l@adchitects.co>
2023-04-29 23:12:45 +0200

    feat: YoutubeVideo component.

---
+++ components/YoutubeVideo/index.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef, useState } from "react";
+import ReactPlayer from "react-player";
+import { io, Socket } from "socket.io-client";
+
+const YoutubeVideo: React.FC = () => {
+  const [domLoaded, setDomLoaded] = useState<boolean>(false);
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+  const playerRef = useRef<ReactPlayer | null>(null);
+  const socketRef = useRef<Socket | null>(null);
+
+  const onPlay = () => {
+    const interval = setInterval(() => {
+      setCurrentTime(playerRef.current?.getCurrentTime() || 0);
+    }, 1000);
+    setIntervalId(interval);
+  };
+
+  const onPause = () => {
+    if (intervalId) clearInterval(intervalId);
+  };
+
+  useEffect(() => {
+    setDomLoaded(true);
+    socketRef.current = io("http://localhost:3333");
+    return () => {
+      if (socketRef.current) {
+        socketRef.current.close();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socketRef.current?.connected) {
+      socketRef.current.emit("createWatchtime", {
+        watchtime: currentTime,
+      });
+    }
+  }, [currentTime]);
+
+  return (
+    <>
+      {domLoaded && (
+        <div>
+          <ReactPlayer
+            ref={playerRef}
+            url="https://www.youtube.com/watch?v=2x4aEnaVG_4"
+            playing={true}
+            controls={true}
+            onPlay={onPlay}
+            onPause={onPause}
+            width="640px"
+            height="360px"
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default YoutubeVideo;
