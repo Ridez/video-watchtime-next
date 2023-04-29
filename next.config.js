f69e2fd87c225e1d1ceed6b8e90e62798f6b9ac5
Lukasz Lassota <lukasz.l@adchitects.co>
2023-04-30 01:01:49 +0200

    feat: add sass.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "main.scss";`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
